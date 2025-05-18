fa3f5d3663ead5952e618d8bd14e7654a009ddea
Cheljee Mangulabnan <cheljee.mangulabnan@gmail.com>
2025-05-18 16:00:51 +0800

    feat: added hero buttons

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,9 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: [
+    '~/assets/globals.scss'
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -38,7 +40,7 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
+  modules: ['@nuxtjs/google-fonts'],
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,22 @@
 <template>
   <main>
-    <section class="hero">
+    <section class="hero mb-4 mb-md-10">
       <div class="hero-text">
         <h1 class="text-h4 text-sm-h3 text-md-h2 text-lg-h1 font-weight-medium">SNC Medical Center</h1>
         <p class="text-subtitle text-md-h6 font-italic font-weight-regular">Specialized care, personalized for you.</p>
       </div>
+      <div class="d-flex flex-column d-sm-flex flex-sm-row gap">
+        <v-btn elevation="4" raised rounded small class="button">Get Started</v-btn>
+        <v-btn elevation="4" raised rounded small class="button inverted">Book a Consultation</v-btn>
+      </div>
     </section>
     <section class="specialization">
+      <div class="specialization py-8">
+        <h2 class="text-h3 font-weight-bold mb-4">Meet Our Specialists</h2>
+        <p class="">Meet the doctors behind our care—respected specialists who bring years of training,
+          expertise, and leadership
+          in their fields.</p>
+      </div>
     </section>
   </main>
 </template>
@@ -29,6 +39,7 @@ export default {
   justify-content: center;
   position: relative;
   text-align: center;
+  padding: 0 1rem;
 }
 
 .hero-text {
@@ -39,7 +50,18 @@ export default {
 
 .specialization {
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
+}
+
+.gap {
+  gap: 1rem;
+}
+
+@media only screen and (max-width: 600px) {
+  .gap {
+    gap: 0.5rem;
+  }
 }
 </style>
